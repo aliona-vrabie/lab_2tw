345e6d646efa1d3277784a982adf7aa5fb029c80
aliona- vrabie <alionacrigan20@gmail.com>
2020-03-26 07:49:00 +0200

    Add project files.

---
+++ eUseControl/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Logare()
+        {
+            return View();
+        }
+        public ActionResult Inregistrare()
+        {
+            return View();
+        }
+    }
+}
